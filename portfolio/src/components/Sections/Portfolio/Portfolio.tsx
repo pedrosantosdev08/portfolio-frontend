a340558a3acbf53215391754a5457778a8d2720a
Pedro Santos <pedrogsantosdev@outlook.com>
2026-03-18 22:40:41 -0300

    Atualizando erro de tipagem do useState

--- portfolio/src/components/Sections/Portfolio/Portfolio.tsx
+++ portfolio/src/components/Sections/Portfolio/Portfolio.tsx
@@ -1,53 +1,52 @@
 import { useState } from 'react';
- // Importação do novo componente
-
 import "./Portfolio.css";
 
-import CatalogoFilmes from "../../../assets/images/projects/catalogo-filmes-image.png"
+import CatalogoFilmes from "../../../assets/images/projects/catalogo-filmes-image.png";
 import { ProjectCard } from '../../UI/Card/ProjectCard/ProjectCard';
 import { Modal } from '../../UI/Modal/Modal';
 
 export function Portfolio() {
-  const [selectedProject, setSelectedProject] = useState<any | null>(null);
-
   const myProjects = [
     {
+      id: 1,
       title: "Catálogo de Filmes",
       image: CatalogoFilmes, 
       description: "Página para maiores informações sobre filmes e séries",
-      tech: ["HTML, CSS, JavaScript"],
+      tech: ["HTML", "CSS", "JavaScript"],
       fullDetails: "Sistema completo com informações sobre filmes e séries."
     },
     {
+      id: 2,
       title: "Catálogo de Filmes",
       image: CatalogoFilmes, 
       description: "Página para maiores informações sobre filmes e séries",
-      tech: ["HTML, CSS, JavaScript"],
+      tech: ["HTML", "CSS", "JavaScript"],
       fullDetails: "Sistema completo com informações sobre filmes e séries."
     },
     {
+      id: 3,
       title: "Catálogo de Filmes",
       image: CatalogoFilmes, 
       description: "Página para maiores informações sobre filmes e séries",
-      tech: ["HTML, CSS, JavaScript"],
+      tech: ["HTML", "CSS", "JavaScript"],
       fullDetails: "Sistema completo com informações sobre filmes e séries."
     },
-    
-    
   ];
 
+  type ProjectType = typeof myProjects[0];
+  const [selectedProject, setSelectedProject] = useState<ProjectType | null>(null);
+
   return (
-    <section
-    id='portfolio' className="portfolio-container">
+    <section id='portfolio' className="portfolio-container">
       <div className="section-title">
-          <h2 >Meus Projetos</h2>
+          <h2>Meus Projetos</h2>
           <p>Aqui estão os melhores projetos</p>
       </div>
       
       <div className="portfolio-grid">
-        {myProjects.map((project, index) => (
+        {myProjects.map((project) => (
           <ProjectCard 
-            key={index}
+            key={project.id}
             title={project.title}
             image={project.image}
             onClick={() => setSelectedProject(project)} 
